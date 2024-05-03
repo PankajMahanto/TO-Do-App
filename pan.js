09ed9a1eaf2b3d31e6a8243b4523ec34a0e91ebe
pan78m <aryanpankaj78@gmail.com>
2024-05-03 19:41:02 +0600

    To-Do-App

---
+++ pan.js
@@ -0,0 +1,85 @@
+
+// মাউস ইভেন্ট টাইপ এগুলা 
+// const headerElement = document.querySelector('#header');
+
+// headerElement.addEventListener('click', (event) => {
+//     console.log(event);
+// });
+
+// headerElement.addEventListener('dblclick', (event) => {
+//     console.log(event);
+// });
+
+
+// headerElement.addEventListener('mouseover', (event) => {
+//     console.log(event);
+// });
+
+// headerElement.addEventListener('mouseout', (event) => {
+//     console.log(event);
+// });
+
+// headerElement.addEventListener('mousedown', (event) => {
+//     console.log(event);
+// });
+
+
+// headerElement.addEventListener('mouseup', (event) => {
+//     console.log(event);
+// });
+
+
+// headerElement.addEventListener('mouseup', (event) => {
+//     console.log(event);
+// });
+
+
+// headerElement.addEventListener('mouseenter', (event) => {
+//     console.log(event);
+// });
+
+
+// headerElement.addEventListener('mouseleave', (event) => {
+//     console.log(event);
+// });
+
+
+// mouse Enter and over difference
+// একই ভাবে mouse leave and out difference একই ভাবে
+
+// headerElement.addEventListener('mouseleave', (event) => {
+//     console.log(event);
+// });
+
+
+// ইনপুট টাইপ দেখবো
+const inputElement = document.querySelector('input[type="text"]');
+
+// inputElement.addEventListener('keydown',(event)=>{
+// console.log(event);
+// });
+
+// inputElement.addEventListener('keyup',(event)=>{
+// console.log(event);
+// });
+
+// inputElement.addEventListener('keypress',(event)=>{
+// console.log(event);
+// });
+
+// inputElement.addEventListener('focus',(event)=>{
+// console.log(event);
+// });
+
+// inputElement.addEventListener('blur',(event)=>{
+// console.log(event);
+// });
+// inputElement.addEventListener('cut',(event)=>{
+// console.log(event);
+// });
+// inputElement.addEventListener('paste',(event)=>{
+// console.log(event);
+// });
+// inputElement.addEventListener('input',(event)=>{
+// console.log(event);
+// });
